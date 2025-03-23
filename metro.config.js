b8aab1b9898554f38776b76bd3a487e803f32054
ShbhamBane478 <shubhambane435@gmail.com>
2025-03-23 12:31:48 +0530

    Expo route screen setup

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname)
+// 1. Watch all files in the project directory
+config.watchFolders = [path.resolve(__dirname)];
+
+// 2. Configure resolver to understand aliases
+config.resolver.extraNodeModules = {
+  '@': path.resolve(__dirname),
+};
+
 
 module.exports = withNativeWind(config, { input: './global.css' })
