6f5576077167ffffeb0ed644fe989380c685fff0
ShbhamBane478 <shubhambane435@gmail.com>
2025-03-26 21:19:32 +0530

    added search screen with search feature

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,25 +1,33 @@
-import { icons } from '@/constants/icons'
-import React from 'react'
-import { Image, TextInput, View } from 'react-native'
+import { View, TextInput, Image } from "react-native";
 
-interface SearchBarProps {
-  onPress?: () => void;
+import { icons } from "@/constants/icons";
+
+interface Props {
   placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  onPress?: () => void;
 }
 
-const SearchBar = ({onPress , placeholder} : SearchBarProps) => {
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
   return (
-    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
-      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8ff"/>
-      <TextInput 
-      onPress={onPress}
-      placeholder={placeholder} 
-      value=''
-      onChangeText={()=>{}}
-    placeholderTextColor='#a8b5db'
-      className='flex-1 ml-2 text-white'/>
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="w-5 h-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        className="flex-1 ml-2 text-white"
+        placeholderTextColor="#A8B5DB"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
