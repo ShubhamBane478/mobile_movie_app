6a0732d0573bcaa4b733aabb918a74948849c7ff
ShbhamBane478 <shubhambane435@gmail.com>
2025-03-27 21:27:26 +0530

    Appwrite setup with application

---
+++ services/appwrite.ts
@@ -0,0 +1,13 @@
+//track the seraches made by a user 
+
+export const updateSearchCount = async (query: string , movie: Movie) => {
+    //check if  a record fo that search has already been stored
+
+    //if a document is found increament the searchCount filed 
+
+    // if no document is found than 
+        //create a new document in Appwrite databas 
+
+}
+
+// Note : document means to store the data similar to table in Sql
