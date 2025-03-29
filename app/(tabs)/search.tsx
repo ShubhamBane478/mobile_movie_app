5b6ef082469f8668da8e7e3d82d43957b0433b62
ShbhamBane478 <shubhambane435@gmail.com>
2025-03-29 11:42:42 +0530

    add appwrite integration

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,14 +1,18 @@
-import MovieCard from "@/components/MovieCard";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+
+import MovieDisplayCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
-import React, { useEffect } from "react";
-import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+import React from "react";
 
 const Search = () => {
-  const [searchQuery, setSearchQuery] = React.useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
   const {
     data: movies = [],
     loading,
@@ -17,51 +21,62 @@ const Search = () => {
     reset,
   } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+  };
+
+  // Debounced search effect
   useEffect(() => {
-    const timeout = setTimeout(async () => {
-      if (searchQuery.length > 2) {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
         await loadMovies();
+
+        // Call updateSearchCount only if there are results
+        if (movies?.length! > 0 && movies?.[0]) {
+          await updateSearchCount(searchQuery, movies[0]);
+        }
       } else {
         reset();
       }
     }, 500);
 
-    return () => clearTimeout(timeout);
+    return () => clearTimeout(timeoutId);
   }, [searchQuery]);
 
   return (
-    <View className="flex-1 bg-primary ">
+    <View className="flex-1 bg-primary">
       <Image
         source={images.bg}
         className="flex-1 absolute w-full z-0"
         resizeMode="cover"
       />
+
       <FlatList
-        data={movies}
-        renderItem={({ item }) => <MovieCard {...item} />}
-        keyExtractor={(item) => item.id.toString()}
         className="px-5"
+        data={movies as Movie[]}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <MovieDisplayCard {...item} />}
         numColumns={3}
         columnWrapperStyle={{
-          justifyContent: "center",
+          justifyContent: "flex-start",
           gap: 16,
           marginVertical: 16,
         }}
-        contentContainerStyle={{
-          paddingBottom: 100,
-        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
         ListHeaderComponent={
           <>
             <View className="w-full flex-row justify-center mt-20 items-center">
               <Image source={icons.logo} className="w-12 h-10" />
             </View>
+
             <View className="my-5">
               <SearchBar
-                placeholder="Search movie ... "
+                placeholder="Search for a movie"
                 value={searchQuery}
-                onChangeText={(text) => setSearchQuery(text)}
+                onChangeText={handleSearch}
               />
             </View>
+
             {loading && (
               <ActivityIndicator
                 size="large"
@@ -69,18 +84,22 @@ const Search = () => {
                 className="my-3"
               />
             )}
+
             {error && (
               <Text className="text-red-500 px-5 my-3">
-                Error : {error.message}
+                Error: {error.message}
               </Text>
             )}
 
-            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
-              <Text className="text-xl text-white font-bold ">
-                Search Results for{" "}
-                <Text className="text-accent">{searchQuery.trim()}</Text>
-              </Text>
-            )}
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length! > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
           </>
         }
         ListEmptyComponent={
