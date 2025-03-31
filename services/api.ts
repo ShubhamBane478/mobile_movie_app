d05ea4b5229a33e15165ec60fd2b1c17e59e0c9f
ShbhamBane478 <shubhambane435@gmail.com>
2025-03-31 12:33:10 +0530

    add movie details screen

--- services/api.ts
+++ services/api.ts
@@ -1,33 +1,50 @@
 export const TMDB_CONFIG = {
-    BASE_URL: 'https://api.themoviedb.org/3',
-    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  // @ts-ignore
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const fetchMoviesDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  const response = await fetch(
+    `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+    {
+      method: "GET",
+      headers: TMDB_CONFIG.headers,
     }
-}
- 
-export const fetchMovies = async ({query} : {query: string}) => {
-    const endpoint = query ?  `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`: `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  );
 
-    const response = await fetch(endpoint,{
-        method: 'GET',
-        headers: TMDB_CONFIG.headers,
-    })
+  if (!response.ok) throw new Error("Failed to fetch movie details");
 
-    if(!response.ok){
-        // @ts-ignore
-        throw new Error('Failed to fetch movies',response.statusText);
-    }
-
-            // @ts-ignore
-
-    const data  = await response.json();
-
-    return  data.results;
-
-    
-}
+  const data = await response.json();
+  return data;
+};
 
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
 // const options = {
